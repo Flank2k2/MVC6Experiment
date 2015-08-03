872617ae637917e7384b3962ddbaa9b16ef470e4
apoulet <antoin.poulet@gmail.com>
2015-08-03 05:07:55 -0700

    Add missing files

---
+++ src/MVC6Experiment/Service/DocumentService.cs
@@ -0,0 +1,18 @@
+﻿using MVC6Experiment.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC6Experiment.Service
+{
+    public class DocumentService
+    {
+
+        public void GenerateTemplate(Template template)
+        {
+
+        }
+
+    }
+}
